b4427b6aa76a784a999444ff437215832ef208a5
Clément <clement.salomon@etu.univ-amu.fr>
2024-01-31 18:06:14 +0100

    cell color system and rdApple

--- Clément Salomon - Snake Game JS/script.js
+++ Clément Salomon - Snake Game JS/script.js
@@ -1,22 +1,45 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const grid = document.querySelector(".grid");
+    // ---------------------------------------
+    // |              VARIABLES              |
+    // ---------------------------------------
+    let apple = [];
+    let snake = [[14,4],[14,5],[14,6]];
 
+document.addEventListener("DOMContentLoaded", function () {
+    // ----------------------------------------
+    // |                 MAIN                 |
+    // ----------------------------------------
+    const grid = document.querySelector(".grid");
+    console.log(grid);
+    createGrid();
+
+    // ---------------------------------------
+    // |              FUNCTIONS              |
+    // ---------------------------------------
     function createGrid() {
         for (let row = 0; row < 20; row++) {
             for (let col = 0; col < 30; col++) {
                 const cell = document.createElement("div");
-                cell.classList.add("cell");
-                if( (row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1) ) {
-                    cell.style.backgroundColor = '#d1d1d1'
+                if ((row % 2 == 0 && col % 2 == 0) || (row % 2 == 1 && col % 2 == 1)) {
+                    cell.classList.add("darker-cell");
+                } else {
+                    cell.classList.add("lighter-cell");
                 }
-                else {
-                    cell.style.backgroundColor = '#dcdcdc'
-                }
+                cell.id = "cell-" + row + "-" + col;
                 grid.appendChild(cell);
             }
         }
     }
-    createGrid();
-
-    console.log(grid);
 });
+
+    function rdApple() {
+        const randomRow = Math.floor(Math.random() * 20);
+        const randomCol = Math.floor(Math.random() * 30);
+        const randomCell = document.getElementById("cell-" + randomRow + "-" + randomCol);
+        randomCell.classList.add("apple");
+        if (apple.length === 2) {
+            const previousApple = document.getElementById("cell-" + apple[0] + "-" + apple[1]);
+            previousApple.classList.remove("apple");
+        }
+        apple = [randomRow, randomCol];
+    }
+
