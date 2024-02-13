f194b24dcde70735a340403c0541671910e57636
Clément <clement.salomon@etu.univ-amu.fr>
2024-02-13 18:28:57 +0100

    First API call

--- TP2/jsFiles/Main.js
+++ TP2/jsFiles/Main.js
@@ -3,7 +3,10 @@ import { Logger, Logger2, Logger3 } from './logger.js';
 import { switchTab, displayNextValue, changeLabel } from './utils.js';
 
 // Lancer l'application
-function start() {
+async function start() {
+
+    await observable.start();
+
     displayNextValue();
     switchTab(1);
 }
@@ -17,7 +20,5 @@ const logger3 = new Logger3();
 observable.subscribe(logger1);
 observable.subscribe(logger2);
 observable.subscribe(logger3);
-
-observable.notify('Go');
 
 start();
--- TP2/jsFiles/observable.js
+++ TP2/jsFiles/observable.js
@@ -3,16 +3,47 @@ class Observable {
         this.observers = [];
     }
 
+    async fetchData() {
+        try {
+            const response = await fetch('https://hothothot.dog/api/capteurs/exterieur');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            if (data && data.capteurs && data.capteurs.length > 0) {
+                const { type, Nom, Valeur, Timestamp } = data.capteurs[0];
+                return Valeur;
+            } else {
+                throw new Error('No data or invalid data format in response');
+            }
+            return data;
+        } catch (error) {
+            console.error('Error fetching data:', error);
+            return null;
+        }
+    }
+
+    async start() {
+        const data = await this.fetchData();
+        if (data) {
+            console.log('Data from API:', data);
+            // Notify observers with the fetched data
+            this.notify(data);
+        } else {
+            console.log('Failed to fetch data from API');
+        }
+    }
+
     subscribe(observer) {
         this.observers.push(observer);
     }
 
     unsubscribe(observer) {
-        this.observers = this.observers.filter(obs => obs !== observer);
+        // TODO: Implement unsubscribe logic
     }
 
     notify(data) {
-        this.observers.forEach(observer => observer.update(data));
+        this.observers.forEach((observer) => observer.update(data));
     }
 }
 
