9a5d804e29e5685996775df3f9e6543b99b1d775
Clément <clement.salomon@etu.univ-amu.fr>
2024-02-13 12:41:57 +0100

    Système d'onglets  (accessible, cela va de soi !)

--- TP2/script.js
+++ TP2/script.js
@@ -1,14 +1,35 @@
+<!-- Les Variables -->
 let A_tableau = Array.from({ length: 20000 }, () => Math.floor(Math.random() * (40 - -10 + 1)) + -10);
 let I_index = 0;
 const EL_currentTemperatureElement = document.getElementById("currentTemperature");
 const EL_temperatureListElement = document.getElementById("temperatureList");
 const EL_message= document.getElementById("message");
 
+<!-- Les Fonctions -->
+function switchTab(tabIndex) {
+    // Cacher tous les onglets
+    var tabs = document.querySelectorAll('.tab');
+    tabs.forEach(function(tab) {
+        tab.classList.remove('active');
+    });
+    // Désélectionner tous les boutons d'onglet
+    var tabButtons = document.querySelectorAll('.tab-button');
+    tabButtons.forEach(function(button) {
+        button.setAttribute('aria-selected', 'false');
+    });
+    // Afficher l'onglet sélectionné
+    var selectedTab = document.getElementById('tab-content-' + tabIndex);
+    selectedTab.classList.add('active');
+    // Sélectionner le bouton d'onglet correspondant
+    var selectedTabButton = document.querySelector('.tab-button:nth-of-type(' + tabIndex + ')');
+    selectedTabButton.setAttribute('aria-selected', 'true');
+}
+
 function displayNextValue() {
     if (I_index < A_tableau.length) {
         let I_currentTemperature = A_tableau[I_index];
 
-        document.body.style.backgroundColor = getBackgroundColor(I_currentTemperature);
+        changeLabel(I_currentTemperature);
         EL_currentTemperatureElement.innerHTML = "Température actuelle : " + I_currentTemperature + "<abbr>°C</abbr>";
         let el = document.createElement('div');
         el.innerHTML = I_currentTemperature+"<abbr>°C<abbr>";
@@ -24,20 +45,17 @@ function displayNextValue() {
     }
 }
 
-function getBackgroundColor(temperature) {
+function changeLabel(I_currentTemperature) {
     EL_message.textContent = "";
-    if (temperature <= 0) {
+    if (I_currentTemperature <= 0) {
         EL_message.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
-        return "#87CEEB";
     }
-    else if (temperature <= 20) return "#90EE90";
-    else if (temperature <= 30) return "#FFD700";
-    else if (temperature <= 40) {
+    else if (I_currentTemperature <= 40) {
         EL_message.textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
-        return "#FF6347";
     }
-    else return "#f0f0f0";
 }
 
 
+<!-- Le Main() -->
 displayNextValue();
+switchTab(1);
