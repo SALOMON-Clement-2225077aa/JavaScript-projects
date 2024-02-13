ffe77cbb8896a12c9fc051d6dd0c67595c602126
Clément <clement.salomon@etu.univ-amu.fr>
2024-02-13 11:56:30 +0100

    Fix commit precedant

--- TP2/script.js
+++ TP2/script.js
@@ -1,39 +1,39 @@
 let A_tableau = Array.from({ length: 20000 }, () => Math.floor(Math.random() * (40 - -10 + 1)) + -10);
 let I_index = 0;
-const A_currentI_temperatureElement = document.getElementById("A_currentI_temperature");
-const I_temperatureListElement = document.getElementById("I_temperatureList");
-const message= document.getElementById("message");
+const EL_currentTemperatureElement = document.getElementById("currentTemperature");
+const EL_temperatureListElement = document.getElementById("temperatureList");
+const EL_message= document.getElementById("message");
 
 function displayNextValue() {
     if (I_index < A_tableau.length) {
-        let A_currentI_temperature = A_tableau[I_index];
+        let I_currentTemperature = A_tableau[I_index];
 
-        document.body.style.backgroundColor = getBackgroundColor(A_currentI_temperature);
-        A_currentI_temperatureElement.innerHTML = "Température actuelle : " + A_currentI_temperature + "<abbr>°C</abbr>";
+        document.body.style.backgroundColor = getBackgroundColor(I_currentTemperature);
+        EL_currentTemperatureElement.innerHTML = "Température actuelle : " + I_currentTemperature + "<abbr>°C</abbr>";
         let el = document.createElement('div');
-        el.innerHTML = A_currentI_temperature+"<abbr>°C<abbr>";
+        el.innerHTML = I_currentTemperature+"<abbr>°C<abbr>";
 
-        if (A_currentI_temperature <= 0) el.classList.add("blue-border");
-        else if (A_currentI_temperature <= 20) el.classList.add("green-border");
-        else if (A_currentI_temperature <= 30) el.classList.add("orange-border");
-        else if (A_currentI_temperature <= 40) el.classList.add("red-border");
+        if (I_currentTemperature <= 0) el.classList.add("blue-border");
+        else if (I_currentTemperature <= 20) el.classList.add("green-border");
+        else if (I_currentTemperature <= 30) el.classList.add("orange-border");
+        else if (I_currentTemperature <= 40) el.classList.add("red-border");
 
-        I_temperatureListElement.appendChild(el);
+        EL_temperatureListElement.appendChild(el);
         I_index++;
         setTimeout(displayNextValue, 2000);
     }
 }
 
-function getBackgroundColor(I_temperature) {
-    message.textContent = "";
-    if (I_temperature <= 0) {
-        message.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
+function getBackgroundColor(temperature) {
+    EL_message.textContent = "";
+    if (temperature <= 0) {
+        EL_message.textContent = "Brrrrrrr, un peu froid ce matin, mets ta cagoule !";
         return "#87CEEB";
     }
-    else if (I_temperature <= 20) return "#90EE90";
-    else if (I_temperature <= 30) return "#FFD700";
-    else if (I_temperature <= 40) {
-        message.textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
+    else if (temperature <= 20) return "#90EE90";
+    else if (temperature <= 30) return "#FFD700";
+    else if (temperature <= 40) {
+        EL_message.textContent = "Caliente ! Vamos a la playa, ho hoho hoho !!";
         return "#FF6347";
     }
     else return "#f0f0f0";
